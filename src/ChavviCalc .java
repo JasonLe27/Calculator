e51db2d7bb1f82b8d771b66c85f0dd9494cdb88f
JasonLe27 <89886507+JasonLe27@users.noreply.github.com>
2021-10-07 23:43:12 -0400

    Add files via upload
    Final

---
+++ src/ChavviCalc .java
@@ -0,0 +1,16 @@
+public class ChavviCalc {
+    public static void main(String[] args){
+        a = 0;
+        b = 0;
+        while(true)
+        {
+        Scanner myObj = new Scanner(System.in);
+        char x = myObj.nextLine();
+        if( x == 'a' )
+        {
+
+        }
+        }
+
+    }
+}
